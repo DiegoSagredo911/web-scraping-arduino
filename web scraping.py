cf320e111f341d0aad802304d8f6b81a6338b17c
DiegoSagredo911 <71300492+DiegoSagredo911@users.noreply.github.com>
2023-03-17 16:34:56 -0300

    v1

---
+++ web scraping.py
@@ -0,0 +1,42 @@
+from bs4 import BeautifulSoup
+import requests
+import time
+import serial
+import datetime
+#from os import system
+
+#port where the arduino is located
+puerto = 'COM4'
+
+baudrate = 2000000
+ser = serial.Serial(port=puerto, baudrate=baudrate)
+ser.isOpen()
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
+numberFollowedBy = 0
+
+print("⚠️  Only with public accounts⚠️")
+user = input("User name instagram: ")
+try:
+    while(True):
+        
+        response = requests.get(f"https://www.picuki.com/profile/{user}",headers=headers)
+        html = response.content
+        soup = BeautifulSoup(html, 'html.parser')
+        username = soup.find('h1', class_= 'profile-name-top').get_text()
+        name = soup.find('h2', class_= 'profile-name-bottom').get_text()
+        followedBy = soup.find('span', class_= 'followed_by').get_text()
+        follows = soup.find('span', class_= 'follows').get_text()
+
+        if numberFollowedBy!=followedBy:
+            numberFollowedBy = followedBy
+            ser.write(f"{username} +{name} +{followedBy} +{follows} + -- ".encode())
+            #system("cls")
+            print(f"Send data {datetime.datetime.now()}")
+        else:
+            #system("cls")
+            print(f"No Changes {datetime.datetime.now()}")
+        
+        time.sleep(5)
+except:
+    print("Error")
